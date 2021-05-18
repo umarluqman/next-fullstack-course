4ef26c4cc7292b45bc4bc146a3474c3f618a01d9
Umar Luqman <>
2021-05-18 09:40:02 +0800

    Added search box to search address on map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,7 +5,7 @@ import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
 import { HousesQuery_houses } from "src/generated/HousesQuery";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 
 interface iProps {
   setDataBounds: (bounds: string) => void;
@@ -49,6 +49,25 @@ const Map = ({ setDataBounds, houses, highlightedId }: iProps) => {
           }
         }}
       >
+        <div className="absolute top-0 w-full z-10 p-4">
+          <SearchBox
+            defaultValue=""
+            onSelectAddress={(_address, latitude, longitude) => {
+              if (latitude && longitude) {
+                setViewport((old) => ({
+                  ...old,
+                  latitude,
+                  longitude,
+                  zoom: 12,
+                }));
+                if (mapRef.current) {
+                  const bounds = mapRef.current.getMap().getBounds();
+                  setDataBounds(JSON.stringify(bounds.toArray()));
+                }
+              }
+            }}
+          />
+        </div>
         {houses.map((house) => (
           <Marker
             key={house.id}
